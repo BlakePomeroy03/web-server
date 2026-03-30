c973415c1c9e3ee4368ed6e24f037f1bfe2d964f
Blake <blakepomeroy03@gmail.com>
2026-03-30 13:06:56 -0600

    feat: HTTP request routing

--- server.py
+++ server.py
@@ -13,17 +13,35 @@ print("Listening on " + HOST + ":" + str(PORT))
 
 while True:
     client_connection, client_address = server_socket.accept()
-    print("Connection from: " + str(client_address))
-
     raw_bytes = client_connection.recv(1024)
     request_data = raw_bytes.decode('utf-8', errors='replace')
     
-    http_response = (
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/html\r\n"
-        "\r\n"
-        "<html><body><h1>Hello</h1><p>It actually works.</p></body></html>"
-    )
+    if not request_data:
+        client_connection.close()
+        continue
+
+    request_lines = request_data.split('\r\n')
+    first_line = request_lines[0]
+    
+    try:
+        method, path, version = first_line.split(' ')
+    except ValueError:
+        client_connection.close()
+        continue
+        
+    print("User requested path: " + path)
+
+    if path == '/':
+        status = "HTTP/1.1 200 OK\r\n"
+        body = "<html><body><h1>Home Page</h1><p>Welcome to the main dashboard.</p></body></html>"
+    elif path == '/about':
+        status = "HTTP/1.1 200 OK\r\n"
+        body = "<html><body><h1>About Me</h1><p>I built this server from scratch.</p></body></html>"
+    else:
+        status = "HTTP/1.1 404 Not Found\r\n"
+        body = "<html><body><h1>404 Error</h1><p>That page does not exist on this server.</p></body></html>"
+
+    http_response = status + "Content-Type: text/html\r\n\r\n" + body
     
     client_connection.sendall(http_response.encode('utf-8'))
     client_connection.close()
