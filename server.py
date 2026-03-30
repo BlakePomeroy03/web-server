993fb780b20b0f6b31c2afc1dd94930380627939
Blake <blakepomeroy03@gmail.com>
2026-03-30 13:04:16 -0600

    test: HTTP request

--- server.py
+++ server.py
@@ -11,16 +11,19 @@ server_socket.listen(1)
 
 print("Listening on " + HOST + ":" + str(PORT))
 
-client_connection, client_address = server_socket.accept()
-print("Connection from: " + str(client_address))
+while True:
+    client_connection, client_address = server_socket.accept()
+    print("Connection from: " + str(client_address))
 
-raw_bytes = client_connection.recv(1024)
-
-request_data = raw_bytes.decode('utf-8', errors='replace')
-
-print("\n--- INCOMING MESSAGE ---")
-print(request_data)
-print("------------------------\n")
-
-client_connection.close()
-print("Connection closed")
+    raw_bytes = client_connection.recv(1024)
+    request_data = raw_bytes.decode('utf-8', errors='replace')
+    
+    http_response = (
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html\r\n"
+        "\r\n"
+        "<html><body><h1>Hello</h1><p>It actually works.</p></body></html>"
+    )
+    
+    client_connection.sendall(http_response.encode('utf-8'))
+    client_connection.close()
